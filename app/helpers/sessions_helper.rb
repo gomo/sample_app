90630d60cbf819989be1a5ac97ef14477e91a724
Masamoto Miyata <miyata@sincere-co.com>
2017-09-22 07:28:50 +0000

    8.2.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  # 現在ログイン中のユーザーを返す (いる場合)
+  def current_user
+    if @current_user.nil? then
+      @current_user = User.find_by(id: session[:user_id]) || false
+    end
+    @current_user
+  end
+  
+  def logged_in?
+    !!current_user
+  end
 end
