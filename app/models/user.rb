18a99b1a27cb0bd282dae475a293aed174fd91a1
Masamoto Miyata <miyata@sincere-co.com>
2017-09-25 09:06:27 +0000

    Added remember login user function.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  
   before_save { email.downcase! }
   
   validates :name,
@@ -16,6 +18,7 @@ class User < ApplicationRecord
     presence: true,
     length: { minimum: 6 }
 
+  has_secure_password
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
@@ -24,5 +27,24 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
   
-  has_secure_password
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
 end
