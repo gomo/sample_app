962628cf9c8797888476c89c880123d823f8b13a
Masamoto Miyata <miyata@sincere-co.com>
2017-09-25 09:31:37 +0000

    Fixed remember me bugs. Implemeted to update remember_token on use once.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,7 @@ module SessionsHelper
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
+        remember user
         @current_user = user
       end
     end
