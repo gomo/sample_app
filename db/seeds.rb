8cb0454a2d39c145c27e29074cad91a9a980ff9b
Masamoto Miyata <miyata@sincere-co.com>
2017-10-03 18:47:13 +0900

    Added pagenation. Added users test.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,14 @@ User.create([{
   email: "miyata@sincere-co.com",
   password: "111111"
 }])
+
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
